b7e98db5aea1b3b176d45a32482954574f0af0bb
徐祥洲 <862960044@qq.com>
2016-05-31 17:59:14 +0800

    修改了一些bug

--- app/src/main/java/com/example/zzphoneguard/activity/BaseContactsTelSmsActivity.java
+++ app/src/main/java/com/example/zzphoneguard/activity/BaseContactsTelSmsActivity.java
@@ -93,7 +93,7 @@ public abstract class BaseContactsTelSmsActivity extends ListActivity {
                 Message msg = Message.obtain();
                 msg.what=LOADING;
                 handle.sendMessage(msg);
-                SystemClock.sleep(2000);
+                SystemClock.sleep(300);
 //                contacts = ReadContactsEngine.readContacts(getApplicationContext());
                 contacts = getDatas();
                 msg=Message.obtain();
