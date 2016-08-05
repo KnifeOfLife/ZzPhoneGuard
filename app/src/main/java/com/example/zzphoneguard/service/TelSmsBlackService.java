4b462e8a6113e46958bbc44b64cada80deff5dd0
徐祥洲 <862960044@qq.com>
2016-08-05 11:31:22 +0800

    添加了通讯中心的挂断电话并删除电话记录的服务

--- app/src/main/java/com/example/zzphoneguard/service/TelSmsBlackService.java
+++ app/src/main/java/com/example/zzphoneguard/service/TelSmsBlackService.java
@@ -5,17 +5,26 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
 import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
 
+import com.android.internal.telephony.ITelephony;
 import com.example.zzphoneguard.db.BlackNumberDao;
 import com.example.zzphoneguard.db.BlackNumberTable;
+
+import java.lang.reflect.Method;
 
 public class TelSmsBlackService extends Service {
 
     private SmsReceiver receiver;
     private BlackNumberDao dao;
+    private PhoneStateListener listener;
+    private TelephonyManager tm;
 
     public TelSmsBlackService() {
     }
@@ -60,7 +69,54 @@ public class TelSmsBlackService extends Service {
         IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
         filter.setPriority(Integer.MAX_VALUE);
         registerReceiver(receiver,filter);
-        //注册电话的监听广播
+        //注册电话的监听 采用TelephonyManager类
+        tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        listener = new PhoneStateListener(){
+            @Override
+            public void onCallStateChanged(int state, final String incomingNumber) {
+                super.onCallStateChanged(state, incomingNumber);
+                switch (state){
+                    case TelephonyManager.CALL_STATE_IDLE://挂断的状态
+
+                        break;
+                    case TelephonyManager.CALL_STATE_RINGING://响铃的状态
+                        int mode = dao.getMode(incomingNumber);
+                        if ((mode&BlackNumberTable.TEL)!=0){
+                            System.out.println("挂断电话");
+                            //挂断电话之前注册内容观察者
+                            getContentResolver().registerContentObserver(Uri.parse("content://call_log/calls"), true,
+                                    new ContentObserver(new Handler()) {
+                                        @Override
+                                        public void onChange(boolean selfChange) {
+                                            super.onChange(selfChange);
+                                            deleteCallLog(incomingNumber);
+                                            getContentResolver().unregisterContentObserver(this);
+                                        }
+                                    });
+                            endCall();
+                        }
+                        break;
+                    case TelephonyManager.CALL_STATE_OFFHOOK://通话的状态
+
+                        break;
+                    default:
+                        break;
+
+
+                }
+            }
+        };
+        tm.listen(listener,PhoneStateListener.LISTEN_CALL_STATE);
+
+    }
+
+    /**
+     * 删除电话日志
+     * @param incomingNumber
+     */
+    private void deleteCallLog(String incomingNumber) {
+        Uri uri = Uri.parse("content://call_log/calls");
+        getContentResolver().delete(uri,"number=?",new String[]{incomingNumber});
 
     }
 
@@ -69,7 +125,24 @@ public class TelSmsBlackService extends Service {
         super.onDestroy();
         //取消短信的监听广播
         unregisterReceiver(receiver);
-        //取消电话的监听广播
+        //取消电话的监听
+        tm.listen(listener,PhoneStateListener.LISTEN_NONE);
 
+    }
+
+    /**
+     * 挂断电话
+     */
+    protected void endCall(){
+        try {
+            Class cla = Class.forName("android.os.ServiceManager");
+            Method method = cla.getDeclaredMethod("getService", String.class);
+            IBinder iBinder = (IBinder) method.invoke(null,Context.TELEPHONY_SERVICE);
+            ITelephony iTelephony = ITelephony.Stub.asInterface(iBinder);
+            iTelephony.endCall();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
